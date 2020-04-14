00ea07ceeeb98322da10019614f101e41c116b2c
Nicole Ticknor <ticknor.nicole@gmail.com>
2020-04-14 00:02:01 +0000

    added two types of redirect functionality

--- express_server.js
+++ express_server.js
@@ -43,7 +43,9 @@ app.get('/urls', (req, res) => {
 
 app.post('/urls', (req, res) => {
   console.log(req.body); //log the POST request body to the console
-  res.send("Ok"); // respond with "Ok" (tbr)
+  let shortURL = generateRandomString();
+  urlDatabase[shortURL] = req.body.longURL;
+  res.redirect(`/urls/${shortURL}`);
 });
 
 app.get('/urls/new', (req, res) => {
@@ -55,3 +57,7 @@ app.get('/urls/:shortURL', (req, res) => {
   res.render('urls_show', templateVars);
 });
 
+app.get('/u/:shortURL', (req, res) => {
+  const longURL = urlDatabase[req.params.shortURL];
+  res.redirect(longURL);
+});
