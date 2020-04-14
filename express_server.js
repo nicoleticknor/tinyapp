ea6c2bf8dd70f43c92967dd48f3da797d61cf639
Nicole Ticknor <ticknor.nicole@gmail.com>
2020-04-14 19:19:42 +0000

    server rebuild from scratch

--- express_server.js
+++ express_server.js
@@ -1,19 +1,6 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const PORT = 8080;
-const bodyParser = require('body-parser');
-app.use(bodyParser.urlencoded({ extend: true }));
-
-app.set('view engine', 'ejs')
-
-function generateRandomString() {
-  const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-  let result = '';
-  for (let i = 1; i <= 6; i++) {
-    result += chars.charAt(Math.floor(Math.random() * chars.length));
-  }
-  return result;
-}
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -21,48 +8,9 @@ const urlDatabase = {
 };
 
 app.get("/", (req, res) => {
-  res.send('Hello!');
+  res.send("Hello!");
 });
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
-});
-
-app.get('/urls.json', (req, res) => {
-  res.json(urlDatabase);
-});
-
-app.get('/hello', (req, res) => {
-  res.send('<html><body>Hello <b>World</b></body></html>\n');
-});
-
-app.get('/urls', (req, res) => {
-  let templateVars = { urls: urlDatabase };
-  res.render('urls_index', templateVars);
-});
-
-app.post('/urls', (req, res) => {
-  console.log(req.body); //log the POST request body to the console
-  let shortURL = generateRandomString();
-  urlDatabase[shortURL] = req.body.longURL;
-  res.redirect(`/urls/${shortURL}`);
-});
-
-app.get('/urls/new', (req, res) => {
-  res.render('urls_new');
-});
-
-app.get('/urls/:shortURL', (req, res) => {
-  let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
-  res.render('urls_show', templateVars);
-});
-
-app.get('/u/:shortURL', (req, res) => {
-  const longURL = urlDatabase[req.params.shortURL];
-  res.redirect(longURL);
-});
-
-app.post('/urls/:shortURL/delete', (req, res) => {
-  delete urlDatabase[req.params.shortURL];
-  console.log(urlDatabase);
 });
